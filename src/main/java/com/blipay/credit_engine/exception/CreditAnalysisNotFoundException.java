9d4571138fa7090f6b8712d0b46b5ceadb641932
Julio Baptistella <juliocbaptistella@gmail.com>
2025-01-26 20:30:45 -0300

    Add config exception

--- src/main/java/com/blipay/credit_engine/exception/CreditAnalysisNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.blipay.credit_engine.exception;
-
-public class CreditAnalysisNotFoundException extends Exception {
-
-    public CreditAnalysisNotFoundException(String documentNumber) {
-        super(String.format("Already exists credit analysis with document number: %s", documentNumber));
-    }
-
-}
